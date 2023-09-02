8ba3eebe6db045de40c411a1a98772fc2ced7dd8
libondev <bon.li@outlook.com>
2023-09-02 14:38:37 +0800

    feat: Make full build of animate.css optional

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,25 @@ import { type Preset } from 'unocss'
 import { animatedRules } from './animated'
 import animatedName from './animatedName.json'
 
-export function animatedUno(): Preset<any> {
+interface Options {
+  /**
+   * Whether to output the entire animate.css.
+   * Which is very effective if you need to dynamically combine class names at runtime.
+   * @default true
+   */
+  buildFullVersion: boolean
+}
+
+export function animatedUno({
+  buildFullVersion = true,
+} = ({} as Options)): Preset<any> {
   return {
     name: 'animated-unocss',
     rules: [
       ...animatedRules,
     ],
-    safelist:animatedName.map(name=>'animated-'+name),
+    safelist: buildFullVersion
+      ? animatedName.map(name => `animated-${name}`)
+      : [],
   }
 }
