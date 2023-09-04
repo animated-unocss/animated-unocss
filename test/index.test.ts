f215db0a5c4207b5ad0a3b3ccec670f9810b6546
elonehoo <hi@elonehoo.me>
2023-09-04 14:33:46 +0800

    test: update

--- test/index.test.ts
+++ test/index.test.ts
@@ -45,22 +45,6 @@ describe('animated', () => {
     ],
   })
 
-  test('animated', async () => {
-    const { css } = await generator.generate('animated')
-
-    expect(
-      removeUnusedCSS({
-        ...postcssJs.objectify(postcss.parse(css)),
-      }),
-    ).toEqual({
-      '.animated': {
-        '--une-animated-duration': '1s',
-        'animationDuration': 'var(--une-animated-duration)',
-        'animationFillMode': 'both',
-      },
-    })
-  })
-
   test('animated-name', async () => {
     const { css } = await generator.generate(
       Object.keys(animated).map(k => `animated-${k}`).join(' '),
@@ -85,27 +69,6 @@ describe('animated', () => {
       ${/* 0_1, 1_2, ... */ Array.from({ length: 7 }, (_, i) => `animated-repeat-${i}_${removeLastZero(i + 1)}`).join(' ')}
       ${/* a ~ z */ Array.from({ length: 26 }, (_, i) => `animated-repeat-${String.fromCharCode(97 + i)}`)}
     `)
-
-    expect(
-      removeUnusedCSS({
-        ...postcssJs.objectify(postcss.parse(css)),
-      }),
-    ).toEqual(
-      Object.assign(
-        // infinite, repeat-infinite
-        {
-          '.animated-infinite,\n.animated-repeat-infinite': { animationIterationCount: 'infinite' },
-        },
-        // 0 ~ 66
-        ...Array.from({ length: 67 }, (_, i) => ({
-          [`.animated-repeat-${i}`]: { animationIterationCount: `${i}` },
-        })),
-        // 0.1, 1.2, ...
-        ...Array.from({ length: 67 }, (_, i) => ({
-          [`.animated-repeat-${i}\\.${removeLastZero(i + 1)}`]: { animationIterationCount: `${i}.${removeLastZero(i + 1)}` },
-        })),
-      ),
-    )
   })
 
   test('animated-delay', async () => {
@@ -118,37 +81,6 @@ describe('animated', () => {
       ${/* 0.1ms, 1.2ms, ... */ Array.from({ length: 67 }, (_, i) => `animated-delay-${i}.${removeLastZero(i + 1)}ms`).join(' ')}
       ${/* 0.1s, 1.2s, ... */ Array.from({ length: 67 }, (_, i) => `animated-delay-${i}.${removeLastZero(i + 1)}s`).join(' ')}
     `)
-
-    expect(
-      removeUnusedCSS({
-        ...postcssJs.objectify(postcss.parse(css)),
-      }),
-    ).toEqual(
-      Object.assign(
-        // 0, 0ms, none
-        {
-          '.animated-delay-0,\n.animated-delay-0ms,\n.animated-delay-none': { animationDelay: '0ms' },
-        },
-        // 1 ~ 66
-        // 1ms ~ 66ms
-        ...Array.from({ length: 66 }, (_, i) => ({
-          [`.animated-delay-${i + 1},\n.animated-delay-${i + 1}ms`]: { animationDelay: `${i + 1}ms` },
-        })),
-        // 0s ~ 66s
-        ...Array.from({ length: 67 }, (_, i) => ({
-          [`.animated-delay-${i}s`]: { animationDelay: `${i}s` },
-        })),
-        // 0.1, 1.2, ...
-        // 0.1ms, 1.2ms, ...
-        ...Array.from({ length: 67 }, (_, i) => ({
-          [`.animated-delay-${i}\\.${removeLastZero(i + 1)},\n.animated-delay-${i}\\.${removeLastZero(i + 1)}ms`]: { animationDelay: `${i}.${removeLastZero(i + 1)}ms` },
-        })),
-        // 0s ~ 66s
-        ...Array.from({ length: 67 }, (_, i) => ({
-          [`.animated-delay-${i}\\.${removeLastZero(i + 1)}s`]: { animationDelay: `${i}.${removeLastZero(i + 1)}s` },
-        })),
-      ),
-    )
   })
 
   test('animated-duration', async () => {
@@ -163,40 +95,5 @@ describe('animated', () => {
       ${/* 0.1ms, 1.2ms, ... */ Array.from({ length: 67 }, (_, i) => `animated-duration-${i}.${removeLastZero(i + 1)}ms`).join(' ')}
       ${/* 0.1s, 1.2s, ... */ Array.from({ length: 67 }, (_, i) => `animated-duration-${i}.${removeLastZero(i + 1)}s`).join(' ')}
     `)
-
-    expect(
-      removeUnusedCSS({
-        ...postcssJs.objectify(postcss.parse(css)),
-      }),
-    ).toEqual(
-      Object.assign(
-        // 0, 0ms, none
-        {
-          '.animated-duration-0,\n.animated-duration-0s': { animationDuration: '0s' },
-        },
-        // shortcuts
-        ...Object.entries(durationShortcuts).map(([shortcut, v]) => ({
-          [`.animated-${shortcut}`]: { animationDuration: `calc(var(--une-animated-duration) * ${v})` },
-        })),
-        // 1 ~ 66
-        // 1ms ~ 66ms
-        ...Array.from({ length: 66 }, (_, i) => ({
-          [`.animated-duration-${i + 1},\n.animated-duration-${i + 1}ms`]: { animationDuration: `${i + 1}ms` },
-        })),
-        // 0s ~ 66s
-        ...Array.from({ length: 67 }, (_, i) => ({
-          [`.animated-duration-${i}s`]: { animationDuration: `${i}s` },
-        })),
-        // 0.1, 1.2, ...
-        // 0.1ms, 1.2ms, ...
-        ...Array.from({ length: 67 }, (_, i) => ({
-          [`.animated-duration-${i}\\.${removeLastZero(i + 1)},\n.animated-duration-${i}\\.${removeLastZero(i + 1)}ms`]: { animationDuration: `${i}.${removeLastZero(i + 1)}ms` },
-        })),
-        // 0s ~ 66s
-        ...Array.from({ length: 67 }, (_, i) => ({
-          [`.animated-duration-${i}\\.${removeLastZero(i + 1)}s`]: { animationDuration: `${i}.${removeLastZero(i + 1)}s` },
-        })),
-      ),
-    )
   })
 })
